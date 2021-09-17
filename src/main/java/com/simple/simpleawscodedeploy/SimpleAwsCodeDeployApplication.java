1e022bf07de4b01da2a7b5cfe264b2166b5c7cd5
Gaurav Kumar <roygaurav000@gmail.com>
2021-09-17 21:56:01 +0530

    first commit

---
+++ src/main/java/com/simple/simpleawscodedeploy/SimpleAwsCodeDeployApplication.java
@@ -0,0 +1,13 @@
+package com.simple.simpleawscodedeploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleAwsCodeDeployApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleAwsCodeDeployApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/simple/simpleawscodedeploy/controller/ApiController.java
@@ -0,0 +1,17 @@
+package com.simple.simpleawscodedeploy.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ApiController 
+{
+	@GetMapping("api/health")
+	public ResponseEntity<?> healthCheck()
+	{
+		return ResponseEntity.ok("It works successfully");
+		
+	}
+	
+}
